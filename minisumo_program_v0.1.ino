182fec536f90ebd8818cc7de1527d2d4f58ebae9
havlian22 <122100034+havlian22@users.noreply.github.com>
2023-01-09 20:21:20 +0100

    Update minisumo_program_v0.1.ino

--- minisumo_program_v0.1.ino
+++ minisumo_program_v0.1.ino
@@ -1,32 +1,74 @@
+//"knihovny"
+#include <BH1750.h>
 #include <VL53L0X.h>
-
+#include "pole_vzdalenosti.h"
+//"motory dopredu"
+//"piny pouze prozatimni"
 #define M1 2
 #define M2 3
+//"motory dozadu"
+#define M1p 4
+#define M2p 5
 
-int cara;
-VL53L0X sensor;
+float svetlo;
+float vzdalenost;
+
+//"vytvoreni senzoru"
+VL53L0X sensorvz;
+BH1750 intenzita_svetla;
 
 void setup() {
-
-  sensor.setTimeout(10);
   
+  //"nastaveni senzoru"
+  sensorvz.setTimeout(1);
+  intenzita_svetla.begin();
+  
+  //"nastaveni vsech motoru na vystup"
   pinMode(M1, OUTPUT);
   pinMode(M2, OUTPUT);
+  pinMode(M1p, OUTPUT);
+  pinMode(M2p, OUTPUT);
 
 }
-// "zjistovani vzdalenosti"
- int distance = sensor.readRangeContinuousMillimeters();
-
+void zjistovani_svetla(){
+  svetlo = intenzita_svetla.readLightLevel();
+}
+  
+//"jed dokud neuvidis tmu"
 void pohyb(){
-  while(cara == 0){
-    digitalWrite(M1, HIGH);
-    digitalWrite(M2, HIGH);
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  
+  while(svetlo>500){
+    zjistovani_svetla(); 
   }
   digitalWrite(M1, LOW);
   digitalWrite(M2, LOW);
 }
 
 void loop() {
-  
+  pohyb();
 
+  digitalWrite(M1p, HIGH);
+  digitalWrite(M2, HIGH);
+  while(vzdalenost > 770){
+    vzdalenost = sensorvz.readRangeContinuousMillimeters();
+  }
+  zjistovani_svetla(); 
+  if(svetlo < 500){
+    
+  }
+  else{
+    int i=0;
+    while(vzdalenost < 770){
+      pole_vzdalenosti [i] = vzdalenost;
+      i++;
+    }
+  }
+
+
+
+
+
+  
 }
