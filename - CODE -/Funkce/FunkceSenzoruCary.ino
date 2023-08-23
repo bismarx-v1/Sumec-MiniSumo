fe77d32907faff96fe8b16209a6d3b90a7d947f5
Mr_Simak <96493198+MrSimak@users.noreply.github.com>
2023-08-23 12:11:13 +0200

    Update FunkceSenzoruCary.ino
    ZMENA:
    -STARY
    -- 'FL' = predni levy, 'FR' = predni pravy, 'BL' = zadni levy, 'BR' = zadni pravy
    
    -NOVY
    --1 = predni levy, 2 = predni pravy, 3 = zadni levy, 4' = zadni pravy

--- - CODE -/Funkce/FunkceSenzoruCary.ino
+++ - CODE -/Funkce/FunkceSenzoruCary.ino
@@ -1,8 +1,6 @@
-
-
-bool LINE_Get(char LINE_Sensor, int LINE_Threshold) {  //input funkce je 'FL' = predni levy, 'FR' = predni pravy, 'BL' = zadni levy, 'BR' = zadni pravy
+bool LINE_Get(int LINE_Sensor, int LINE_Threshold) {  //input funkce je 1 = predni levy, 2 = predni pravy, 3 = zadni levy, 4 = zadni pravy
                               //return: true = bila, false = cerna
-  if(LINE_Sensor=='FL') {
+  if(LINE_Sensor==1) {
     if(analogRead(32)<LINE_Threshold){  //pin leveho predniho senzoru cary (z pohledu sumce)
       return true;
     }
@@ -11,37 +9,46 @@ bool LINE_Get(char LINE_Sensor, int LINE_Threshold) {  //input funkce je 'FL' = 
     }
     
   }
-  
-  if(LINE_Sensor=='FR') {
-    if(analogRead(33)<LINE_Threshold){  //pin praveho predniho senzoru cary (z pohledu sumce)
-      return true;
-    }
-    else {
-      return false;
-    }
-    
-  }
-  
-  if(LINE_Sensor=='BL') {
-    if(analogRead(34)<LINE_Threshold){  //pin leveho zadniho senzoru cary (z pohledu sumce)
-      return true;
-    }
-    else {
-      return false;
+
+  else {
+    if(LINE_Sensor==2) {
+      if(analogRead(33)<LINE_Threshold){  //pin praveho predniho senzoru cary (z pohledu sumce)
+        return true;
+      }
+      else {
+        return false;
+      }
+      
     }
     
-  }
-  
-  if(LINE_Sensor=='BR') {
-    if(analogRead(35)<LINE_Threshold){  //pin praveho zadniho senzoru cary (z pohledu sumce)
-      return true;
-    }
     else {
-      return false;
+      if(LINE_Sensor==3) {
+        if(analogRead(34)<LINE_Threshold){  //pin leveho zadniho senzoru cary (z pohledu sumce)
+          return true;
+        }
+        else {
+          return false;
+        }
+        
+      }
+      
+      else {
+        if(LINE_Sensor==4) {
+          if(analogRead(35)<LINE_Threshold){  //pin praveho zadniho senzoru cary (z pohledu sumce)
+            return true;
+          }
+          else {
+            return false;
+          }
+          
+        }
+        else {
+          return false;
+          Serial.println("Error Line 47 wrong input. ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ")
+        }
+      }
     }
-    
   }
-  
 }
 
  
@@ -54,9 +61,10 @@ void setup() {
 
 
 void loop() {
-  Serial.println(LINE_Get('FL', 3500));
-  Serial.println(LINE_Get('FR', 3500));
-  Serial.println(LINE_Get('BL', 3500));
-  Serial.println(LINE_Get('BR', 3500));
-  Serial.println('====================\n');
+  Serial.println(LINE_Get(1, 3500));
+  Serial.println(LINE_Get(2, 3500));
+  Serial.println(LINE_Get(3, 3500));
+  Serial.println(LINE_Get(4, 3500));
+  Serial.println("====================");
+  delay(1000);
 }
