c53c3b6e11f0846c1c55b915543431544ad9ce36
Mr_Simak <96493198+MrSimak@users.noreply.github.com>
2023-09-04 11:08:30 +0200

    Add files via upload

--- - CODE -/Funkce/FunkceSenzoruCary/line.h
+++ - CODE -/Funkce/FunkceSenzoruCary/line.h
@@ -1,51 +1,57 @@
-bool LINE_Get(int LINE_Sensor, int LINE_Threshold) {  //input funkce je 1 = predni levy, 2 = predni pravy, 3 = zadni levy, 4 = zadni pravy
-                              //return: true = bila, false = cerna
-  if(LINE_Sensor==1) {
-    if(analogRead(32)<LINE_Threshold){  //pin leveho predniho senzoru cary (z pohledu sumce)
-      return true;
-    }
-    else {
-      return false;
-    }
-    
-  }
-
-  else {
-    if(LINE_Sensor==2) {
-      if(analogRead(33)<LINE_Threshold){  //pin praveho predniho senzoru cary (z pohledu sumce)
-        return true;
-      }
-      else {
-        return false;
-      }
-      
-    }
-    
-    else {
-      if(LINE_Sensor==3) {
-        if(analogRead(34)<LINE_Threshold){  //pin leveho zadniho senzoru cary (z pohledu sumce)
-          return true;
-        }
-        else {
-          return false;
-        }
-        
-      }
-      
-      else {
-        if(LINE_Sensor==4) {
-          if(analogRead(35)<LINE_Threshold){  //pin praveho zadniho senzoru cary (z pohledu sumce)
-            return true;
-          }
-          else {
-            return false;
-          }
-          
-        }
-        else {
-          
-        }
-      }
-    }
-  }
+int LINE_Get(int LINE_Sensor, int LINE_Threshold, int LINE_Mode) {
+	switch(LINE_Sensor) {
+		case 1:	//Front Left
+			switch(LINE_Mode) {	//Mode
+				case 0:	//Mode 0
+					return (analogRead(32)<LINE_Threshold ? 1: 0);
+					break;
+					
+				case 1:	//Mode 1
+					return analogRead(32);
+					break;
+			}
+			break;
+			
+		case 2:	//Front Right
+			switch(LINE_Mode) {	//Mode
+				case 0:	//Mode 0
+					return (analogRead(33)<LINE_Threshold ? 1: 0);
+					break;
+					
+				case 1:	//Mode 1
+					return analogRead(33);
+					break;
+			}
+			break;
+			
+		case 3:	//Back Left
+			switch(LINE_Mode) {	//Mode
+				case 0:	//Mode 0
+					return (analogRead(34)<LINE_Threshold ? 1: 0);
+					break;
+					
+				case 1:	//Mode 1
+					return analogRead(34);
+					break;
+			}
+			break;
+			
+		case 4:	//Back Right
+			switch(LINE_Mode) {	//Mode
+				case 0:	//Mode 0
+					return (analogRead(35)<LINE_Threshold ? 1: 0);
+					break;
+					
+				case 1:	//Mode 1
+					return analogRead(35);
+					break;
+			}
+			break;
+		
+		default:
+			return 0;
+			break;
+		
+	}
+	return 0;
 }
