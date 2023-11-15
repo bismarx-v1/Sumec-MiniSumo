c7049c9c0462cce164efc796c9ebc6f07e90a05a
Mr_Simak <matej.simacek.2@gmail.com>
2023-11-15 15:17:31 +0100

    TfL Lib

--- - CODE -/FunkceProEsp32S3/FunkceTfLEsp32S3/TfLunaEsp32S3.h
+++ - CODE -/FunkceProEsp32S3/FunkceTfLEsp32S3/TfLunaEsp32S3.h
@@ -11,12 +11,72 @@ void TfL_Setup() {
 	digitalWrite(41, LOW);
 	digitalWrite(42, LOW);
 	
+	pinMode(15, OUTPUT);
+	
     Wire.begin(18,17,400000);
 	return
 }
 
 void TfL_SetAddrs() {
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
 	
+	while(TfL_Succ==0) {
+		TfL_Succ = tflI2C.Set_I2C_Addr(tfNewAddress1, tfAddr);	//only left sensor should be connected
+		Serial.println(TfL_Succ);
+		delay(1000);
+	}
+	Serial.println("0x11");
+	TfL_Succ = 0;
+	
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
+	
+	while(TfL_Succ==0) {
+		TfL_Succ = tflI2C.Set_I2C_Addr(tfNewAddress2, tfAddr);	//only left and mid sensors should be connected
+		Serial.println(TfL_Succ);
+		delay(1000);
+	}
+	Serial.println("0x12");
+	TfL_Succ = 0;
+	
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
+	
+	while(TfL_Succ==0) {
+		TfL_Succ = tflI2C.Set_I2C_Addr(tfNewAddress3, tfAddr);	//all sensors should be connected
+		Serial.println(TfL_Succ);
+		delay(1000);
+	}
+	Serial.println("0x13");
+	TfL_Succ = 0;
+	
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
+	digitalWrite(15, HIGH);
+	delay(100);
+	digitalWrite(15, LOW);
+	delay(100);
 	
 	return
 }
