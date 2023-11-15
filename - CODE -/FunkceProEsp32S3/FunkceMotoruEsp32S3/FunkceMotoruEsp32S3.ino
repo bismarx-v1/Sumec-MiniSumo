2eae8ebf86146a6fb4594e5da27e6c64c43ac9fb
Mr_Simak <matej.simacek.2@gmail.com>
2023-11-15 19:34:18 +0100

    Some shitvvyujikuijkuhui

--- - CODE -/FunkceProEsp32S3/FunkceMotoruEsp32S3/FunkceMotoruEsp32S3.ino
+++ - CODE -/FunkceProEsp32S3/FunkceMotoruEsp32S3/FunkceMotoruEsp32S3.ino
@@ -1,17 +1,46 @@
 #include "motorsEsp32S3.h"
 
+const int LEDPin = 15;
+TaskHandle_t Task1;
+
+void CodeForTask1(void * parameter) {	/*Code for core 0*/
+	Serial.print("Core ["); Serial.print(xPortGetCoreID()); Serial.println("] started");	//print this core
+	
+	for(;;) {	//void loop()
+		digitalWrite(LEDPin, 1);
+		delay(200);
+		digitalWrite(LEDPin, 0);
+		delay(400);
+	}
+}
+
+
+
 //normalni void setup() s funkci MOTORS_Setup()
 void setup() {
   MOTORS_Setup();
-
+  pinMode(LEDPin, OUTPUT);
+  
+  
+  xTaskCreatePinnedToCore(	/*This command should be at the end of void setup()*/
+		CodeForTask1, 	/*Task Function*/
+		"Task_1", 		/*Name*/
+		3500, 			/*Stack size*/
+		NULL, 			/*Parameter*/
+		0, 				/*Priority*/
+		&Task1, 		/*Task handle*/
+		0);				/*Core*/
 }
 
 
 //normalni void loop()
 void loop() {
-  MOTORS_Go(255/2, -255/2);
+  /*MOTORS_Go(255/2, -255/2);
   delay(2000);
   
+  MOTORS_Go(255/2, 255/2);
+  delay(3000);
+  
   MOTORS_Go(0, 0);
-  delay(2000);
+  delay(2000);*/
 }
