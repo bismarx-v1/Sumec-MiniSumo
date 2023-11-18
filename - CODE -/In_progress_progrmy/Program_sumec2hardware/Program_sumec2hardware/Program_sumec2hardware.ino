33648c0d9ec01aa4b5e5881c73aaed46dad40460
havlian22 <122100034+havlian22@users.noreply.github.com>
2023-11-18 10:43:38 +0100

    khm

--- - CODE -/In_progress_progrmy/Program_sumec2hardware/Program_sumec2hardware/Program_sumec2hardware.ino
+++ - CODE -/In_progress_progrmy/Program_sumec2hardware/Program_sumec2hardware/Program_sumec2hardware.ino
@@ -6,38 +6,59 @@
 #include "TfLunaEsp32S3.h"
 
 #define tlacitko 2
-
-//LED
 #define led 15
+//#define RC5 0
+
+#if defined (RC5)
+    #define ADDR_PROGRAM  11
+    #define ADDR_START    7
+#else
+    #define ADDR_PROGRAM  0
+    #define ADDR_START    3
+#endif
+
+// defines for demo
+#define Button 13
+int Sensor1 = 0;
+int Sensor2 = 0;
+int Sensor3 = 0;
+int Range = 50;  //|sensor range setting||sensor range setting||sensor range setting||sensor range setting||sensor range setting|
+int Sensor = 0;
+int ButtonDown = 0;
+
+// časovo ledkové proměné
+int led_control = 0;
+int cas_zaznam = 0;
+int zapvyp = 1;
+
+int stop = 1;
+// promněná určující mód programu
+int Global_ModeSelectvar = 0;
+int rady = 0;
 const int LEDPin = 15;
 TaskHandle_t Task1;
 int LEDBlink = 0;
 
 //IR
 
-  int code;
-  int adres;
-  int comand_first;
-  int comand;
-  int DohaioID;
-  int start_control = 0;
+int code;
+int adres;
+int comand_first;
+int comand;
+int DohaioID;
+int start_control = 0;
 
-  
-void IRstart()
-{
-  while (start_control == 0)
-  {
+void IRstart() {
+  while (start_control == 0) {
+    //IR čekání
+    decode_results results;
 
-
-    
-    decode_results results;        
-
-    if (irrecv.decode(&results)) {  
+    if (irrecv.decode(&results)) {
 
       code = results.value & 0x7FF;
       comand_first = code & 0x3F;
       comand = comand_first / 0b10;
-      adres = code / 0b1000000; 
+      adres = code / 0b1000000;
 
       Serial.println("");
       Serial.print("recieved signal: ");
@@ -62,12 +83,8 @@ void IRstart()
       Serial.println(comand);
       Serial.println("");
       Serial.println("==========");
-      irrecv.resume();
 
-      if(adres == 0)
-      {
-      Serial.println("program!");
-
+      if (adres == ADDR_PROGRAM) {
         DohaioID = comand;
         Serial.print("DohaioID: ");
         Serial.println(DohaioID);
@@ -80,92 +97,55 @@ void IRstart()
         delay(100);
         digitalWrite(led, LOW);
         delay(100);
-
       }
-      if(adres == 3)
-      {
-        if(comand == DohaioID)
-        {
-          Serial.println("jedeme"); 
-          start_control = 1;   
+
+      if (adres == ADDR_START) {
+        if (comand == DohaioID) {
+          Serial.println("jedeme");
+          start_control = 1;
+        } else {
+          Serial.println("špatný code");
         }
-
-        else
-        {
-          Serial.println("špatný code");    
-        }                 
       }
-    }
 
+      // resume IR reception
+      irrecv.resume();
+    }
   }
 }
 
+void CodeForTask1(void* parameter) { /*Code for core 0*/
+  for (;;) {  //void loop()
+    switch (LEDBlink) {
+      case 0:
+        digitalWrite(LEDPin, 0);
+        break;
 
-
-void CodeForTask1(void * parameter) {    /*Code for core 0*/
-    Serial.print("Core ["); Serial.print(xPortGetCoreID()); Serial.println("] started");    //print this core
-    
-    for(;;) {    //void loop()
-        switch(LEDBlink) {
-            case 0:
-                    digitalWrite(LEDPin, 0);
-                break;
-            
-            case 1:
-                digitalWrite(LEDPin, 1);
-                delay(100);
-                digitalWrite(LEDPin, 0);
-                delay(100);
-                break;
-        }
+      case 1:
+        digitalWrite(LEDPin, 1);
+        delay(100);
+        digitalWrite(LEDPin, 0);
+        delay(100);
+        break;
     }
+  }
 }
 //LED
 
-// defines for demo
-#define Button 13
-int Sensor1 = 0;
-int Sensor2 = 0;
-int Sensor3 = 0;
-int Range = 10; //|sensor range setting||sensor range setting||sensor range setting||sensor range setting||sensor range setting|
-int Sensor = 0;
-int ButtonDown = 0;
-
-  // časovo ledkové proměné
-int led_control = 0;
-int cas_zaznam = 0;
-int zapvyp = 1; 
-
-int stop = 1;
-// promněná určující mód programu
-int Global_ModeSelectvar = 0;
-int rady = 0;
-
 // void setup()
-void setup()
-{
+void setup() {
 
+  Serial.begin(9600);
   pinMode(tlacitko, INPUT);
-  xTaskCreatePinnedToCore(CodeForTask1, "Task_1", 3500, NULL, 0, &Task1, 0);                /*Core*/
+  xTaskCreatePinnedToCore(CodeForTask1, "Task_1", 3500, NULL, 0, &Task1, 0); /*Core*/
   irrecv.enableIRIn();
   MOTORS_Setup();
   TfL_Setup();
   TfL_SetAddrs();
   pinMode(led, OUTPUT);
-  Serial.begin(9600);
 
   //IR čekání
   IRstart();
-  
-  //IR KONEC
-
-  for (int i = 0; i++; i == 2000)
-  {
-    MOTORS_Go(-255 / 2, 255 / 2);
-    delay(1);
-  }
-
-  // Serial.println(analogRead(39));
 }
 
 int laser_number;
@@ -175,180 +155,150 @@ int hodnota_cary = 1000;
 int hodnota_cerne_kalibrace;
 int hodnota_bile_kalibrace;
 int kontrolni_hodnota_kalibrace;
-int tolerance_mereni = 100; // tolerance mčření slouží k vyvážení nepřesnosti sensorů
-// void loop
-void loop()
-{
+int tolerance_mereni = 100;  // tolerance mčření slouží k vyvážení nepřesnosti sensorů
+
+void loop() {
 
   LEDBlink = 1;
-
-/*if (digitalRead(Button) == 1)
-  {
-    Global_ModeSelectvar = 1;
-  }
-
-  else
-  {
-    Global_ModeSelectvar = 0;
-  }*/
-
   start_control = 0;
 
-  if(digitalRead(tlacitko) == HIGH)
-  {
+  if (digitalRead(tlacitko) == HIGH) {
     LEDBlink = 0;
     MOTORS_Go(0, 0);
     Serial.println("OK");
     IRstart();
   }
 
+  switch (Global_ModeSelectvar) {
 
-  switch (Global_ModeSelectvar)
-  {
-
-  case 0:
-    //if (LINE_Get(1, hodnota_cary, 0) == 0 && LINE_Get(2, hodnota_cary, 0) == 0)
-    //{
+    case 0:
+      //if (LINE_Get(1, hodnota_cary, 0) == 0 && LINE_Get(2, hodnota_cary, 0) == 0)
+      //{
 
       // třídící proměná
       laser_number = 0;
-      /*Serial.println("uluny");
-      Serial.println(TfL_Get(0x12));
-      Serial.println(TfL_Get(0x11));
-      Serial.println(TfL_Get(0x13));*/
-      
+
+
 
       // třídění laserů pomocí proměné
 
-      if (cas_zaznam == 0)
-      {
-        if (TfL_Get(0x12) < Range && TfL_Get(0x12) > 0)
-        { // přední laser
+      if (cas_zaznam == 0) {
+        if (TfL_Get(0x12) < Range && TfL_Get(0x12) > 0) {  // přední laser
           laser_number = laser_number + 1;
         }
 
-        if (TfL_Get(0x11) < Range && TfL_Get(0x11) > 0)
-        { // levý laser
+        if (TfL_Get(0x11) < Range && TfL_Get(0x11) > 0) {  // levý laser
           laser_number = laser_number + 3;
         }
 
-        if (TfL_Get(0x13) < Range && TfL_Get(0x13) > 0)
-        { // pravý laser
+        if (TfL_Get(0x13) < Range && TfL_Get(0x13) > 0) {  // pravý laser
           laser_number = laser_number + 5;
         }
-
       }
 
       // Serial.println(laser_number);
 
       // rozpohybování Sumce pomocí proměné "laser_number" vzniklé po třídění
-      switch (laser_number)
-      {
+      switch (laser_number) {
 
-      case 0:
-        MOTORS_Go(255, 255);
-        //Serial.println("dopředu");
-        break;
-      case 1:
-        MOTORS_Go(255 , 255);
-        //Serial.println("dopředu2");
-        break;
+        case 0:
+          MOTORS_Go(255, 255);
+          //Serial.println("dopředu");
+          break;
+        case 1:
+          MOTORS_Go(255, 255);
+          //Serial.println("dopředu2");
+          break;
 
-      case 3:
-        MOTORS_Go(-255 / 2, 255 / 2);
-        //Serial.println("strana1");
-        break;
+        case 3:
+          MOTORS_Go(-255 / 2, 255 / 2);
+          //Serial.println("strana1");
+          break;
 
-      case 5:
-        MOTORS_Go(255 / 2, -255 / 2 );
-        //Serial.println("strana2");
-        break;
+        case 5:
+          MOTORS_Go(255 / 2, -255 / 2);
+          //Serial.println("strana2");
+          break;
 
-      case 4:
-        MOTORS_Go(255, 150);
-        //Serial.println("šikmo1");
-        delay(100);
-        break;
+        case 4:
+          MOTORS_Go(255, 150);
+          //Serial.println("šikmo1");
+          delay(100);
+          break;
 
-      case 6:
-        MOTORS_Go(150, 255);
-        //Serial.println("šikmo2");
-        delay(100);
-        break;
+        case 6:
+          MOTORS_Go(150, 255);
+          //Serial.println("šikmo2");
+          delay(100);
+          break;
 
-      case 9:
-        MOTORS_Go(255, 255);
-        //Serial.println("dopředu");
-        break;
+        case 9:
+          MOTORS_Go(255, 255);
+          //Serial.println("dopředu");
+          break;
       }
 
       // možnost zastavení programu pomocí stop proměné
-      while (stop == 0)
-      {
+      while (stop == 0) {
         MOTORS_Go(0, 0);
       }
 
-      if (cas_zaznam > 0)
-      {
+      if (cas_zaznam > 0) {
         delay(1);
         cas_zaznam = cas_zaznam - 1;
       }
-    //}
+      //}
 
-    // dotek bílé čáry levím senzorem
-    if (LINE_Get(1, hodnota_cary, 0) == 1)
-    {
-      MOTORS_Go(255 / 2, -255 / 2 );
-      delay(500);
-      cas_zaznam = 10;
-    }
-    // dotek bílé čáry pravým senzorem
-    if (LINE_Get(2, hodnota_cary, 0) == 1)
-    {
-      MOTORS_Go(-255 / 2, 255 / 2);
-      delay(500);
-      cas_zaznam = 10;
-    }
-    break;
+      // dotek bílé čáry levím senzorem
+      if (LINE_Get(1, hodnota_cary, 0) == 1) {
+        MOTORS_Go(255 / 2, -255 / 2);
+        delay(500);
+        cas_zaznam = 10;
+      }
+      // dotek bílé čáry pravým senzorem
+      if (LINE_Get(2, hodnota_cary, 0) == 1) {
+        MOTORS_Go(-255 / 2, 255 / 2);
+        delay(500);
+        cas_zaznam = 10;
+      }
+      break;
 
-  case 1:
-    Serial.println("v kalibraci");
+    case 1:
+      Serial.println("v kalibraci");
 
-    MOTORS_Go(0, 0);
-    delay(2000);
-
-    hodnota_cerne_kalibrace = LINE_Get(2, hodnota_cary, 1);
-    Serial.println(hodnota_cerne_kalibrace);
-    delay(100);
-
-    while (hodnota_cerne_kalibrace - LINE_Get(2, hodnota_cary, 1) <= hodnota_cerne_kalibrace - tolerance_mereni)
-    {
-      Serial.println(LINE_Get(2, hodnota_cary, 1));
-      MOTORS_Go(255, 255);
-    }
-    hodnota_bile_kalibrace = LINE_Get(2, hodnota_cary, 1);
-
-    kontrolni_hodnota_kalibrace = hodnota_cary;
-    hodnota_cary = hodnota_cerne_kalibrace / 2;
-
-    if (hodnota_bile_kalibrace > hodnota_cary) // pokud bude hraniční hodnota moc vysoká změní se hraniční hodnota na původní hodnotu
-    {
-      hodnota_cary = kontrolni_hodnota_kalibrace; // změna na původní hodnotu
       MOTORS_Go(0, 0);
-      Serial.println("kalibrace nevysla");
-    }
+      delay(2000);
 
-    else
-    {
-      MOTORS_Go(255, 255);
-      Serial.print("Kalibrace úspěšně provedena, aktuální hraniční hodnota:");
-      Serial.println(hodnota_cary);
+      hodnota_cerne_kalibrace = LINE_Get(2, hodnota_cary, 1);
+      Serial.println(hodnota_cerne_kalibrace);
+      delay(100);
 
-      delay(250);
-    }
+      while (hodnota_cerne_kalibrace - LINE_Get(2, hodnota_cary, 1) <= hodnota_cerne_kalibrace - tolerance_mereni) {
+        Serial.println(LINE_Get(2, hodnota_cary, 1));
+        MOTORS_Go(255, 255);
+      }
+      hodnota_bile_kalibrace = LINE_Get(2, hodnota_cary, 1);
+
+      kontrolni_hodnota_kalibrace = hodnota_cary;
+      hodnota_cary = hodnota_cerne_kalibrace / 2;
+
+      if (hodnota_bile_kalibrace > hodnota_cary)  // pokud bude hraniční hodnota moc vysoká změní se hraniční hodnota na původní hodnotu
+      {
+        hodnota_cary = kontrolni_hodnota_kalibrace;  // změna na původní hodnotu
+        MOTORS_Go(0, 0);
+        Serial.println("kalibrace nevysla");
+      }
+
+      else {
+        MOTORS_Go(255, 255);
+        Serial.print("Kalibrace úspěšně provedena, aktuální hraniční hodnota:");
+        Serial.println(hodnota_cary);
+
+        delay(250);
+      }
 
 
-    Global_ModeSelectvar = 0;
-    break;
+      Global_ModeSelectvar = 0;
+      break;
   }
 }
