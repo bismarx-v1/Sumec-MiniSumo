374de22afc78f2009814e1bc50a2d1e42fd03a48
Mr_Simak <matej.simacek.2@gmail.com>
2023-12-08 13:52:35 +0100

    AutoLunas

---
+++ - CODE -/FunkceProEsp32S3/FunkceTfLEsp32S3Automatic/FunkceTfLEsp32S3Automatic.ino
@@ -0,0 +1,33 @@
+#include "TfLunaEsp32S3.h"	//TfL Lib - the lib import
+
+int16_t tfAddr1 = 0x11;	//TfL Lib - first address (usualy left sensor)
+int16_t tfAddr2 = 0x12;	//TfL Lib - second address (usualy mid sensor)
+int16_t tfAddr3 = 0x13;	//TfL Lib - third address (usualy right sensor)
+
+void setup() {
+    Serial.begin(115200);
+	TfL_Setup();	//TfL Lib - libs setup
+	
+	SetAddrLabel:									//|UNTESTED CODE||UNTESTED CODE||UNTESTED CODE||UNTESTED CODE|
+	
+	TfL_SetAddrs();	//TfL Lib - start the process for setting adresses
+	
+	delay(1000);									//|UNTESTED CODE||UNTESTED CODE||UNTESTED CODE||UNTESTED CODE|
+	if(TfL_IsSet()!=1) {							//|UNTESTED CODE||UNTESTED CODE||UNTESTED CODE||UNTESTED CODE|
+		Serial.print("NotSet");
+		goto SetAddrLabel;							//|UNTESTED CODE||UNTESTED CODE||UNTESTED CODE||UNTESTED CODE|
+	}												//|UNTESTED CODE||UNTESTED CODE||UNTESTED CODE||UNTESTED CODE|
+}
+
+void loop() {
+	Serial.print("Left:\t");
+	Serial.println(TfL_Get(tfAddr1));	//TfL Lib - Get from sensor
+	
+	Serial.print("Mid:\t");
+	Serial.println(TfL_Get(tfAddr2));	//TfL Lib - Get from sensor
+	
+	Serial.print("Right:\t");
+	Serial.println(TfL_Get(tfAddr3));	//TfL Lib - Get from sensor
+	Serial.println("=======================");
+	delay(100);
+}
