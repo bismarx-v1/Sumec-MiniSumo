e503cc3da0cd780add6e09ef88ee97fa1ca44346
Natani Simak <matej.simacek.2@gmail.com>
2024-03-23 14:02:54 +0100

    fixed QRE func

--- - CODE -/NewFolderForCode/Mk3Pcb/Functions/QRE1113/QRE1113.h
+++ - CODE -/NewFolderForCode/Mk3Pcb/Functions/QRE1113/QRE1113.h
@@ -1,26 +1,27 @@
 class QRE {
 	private:
+		const uint8_t Sensor_Pin[3] = {10, 9, 8};	// Pins in order: {"left", "right", "rear"}
 		uint8_t Sensor;
 		
 	public:
 		QRE(uint8_t SensorId);
 		uint8_t get();
-		uint8_t Threshold = 3000;
+		uint8_t Threshold = 3000;	// Default threshold
 		
 };
 
 QRE::QRE(uint8_t SensorId) {
-	if(SensorId == 'L') {
-		this->Sensor = 10;
-	} else if(SensorId == 'R') {
-		this->Sensor = 9;
-	} else if(SensorId == 'B') {
-		this->Sensor = 8;
+	if(SensorId == 'L') {			// Left sensor
+		this->Sensor = Sensor_Pin[0];			// Left sensor pin
+	} else if(SensorId == 'R') {	// Right sensor
+		this->Sensor = Sensor_Pin[1];			// Right sensor pin
+	} else if(SensorId == 'B') {	// Rear sensor marked with "B" for "Back", cos "R" is taken
+		this->Sensor = Sensor_Pin[2];			// Rear sensor pin
 	}
 }
 
 uint8_t QRE::get() {
-	if(analogRead(Sensor)<Threshold) {
+	if(analogRead(Sensor)<Threshold) {	// Returns true/false based on "sensor value" and "threshold"
 		return(1);
 	} else {
 		return(0);
--- - CODE -/NewFolderForCode/Mk3Pcb/Functions/QRE1113/QRE1113.ino
+++ - CODE -/NewFolderForCode/Mk3Pcb/Functions/QRE1113/QRE1113.ino
@@ -1,19 +1,19 @@
 #include "QRE1113.h"
 
-QRE QRELeft('L');
-QRE QRERight('R');
-QRE QREBack('B');
+QRE QRELeft('L');	// Create object for left sensor
+QRE QRERight('R');	// Create object for right sensor
+QRE QREBack('B');	// Create object for rear sensor
 
 void setup() {
 	Serial.begin(115200);
-	QRELeft.Threshold = 3000;
-	QRERight.Threshold = 3000;
-	QREBack.Threshold = 3000;
+	QRELeft.Threshold = 3001;	// Default threshold is 3000, but this can override it
+	QRERight.Threshold = 3001;	// Default threshold is 3000, but this can override it
+	QREBack.Threshold = 3001;	// Default threshold is 3000, but this can override it
 }
 
 void loop() {
-	Serial.printf("%i\n", QRELeft.get());
-	Serial.printf("%i\n", QRERight.get());
-	Serial.printf("%i\n", QREBack.get());
+	Serial.printf("Left: %i\n", QRELeft.get());		// Get and print left value
+	Serial.printf("Rear: %i\n", QREBack.get());		// Get and print rear value
+	Serial.printf("Right: %i\n", QRERight.get());	// Get and print right value
 	delay(100);
 }
