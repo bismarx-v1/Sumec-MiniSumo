b2e30056c149a9d08be06d3a656def00a452a3fa
Natani Simak <matej.simacek.2@gmail.com>
2024-03-23 14:04:55 +0100

    added motor driver func ALL THREE NEW FUNCTIONS ARE UNTESTED BUT COMPILE

--- - CODE -/NewFolderForCode/Mk3Pcb/Functions/DRV8874PWPR/DRV8874PWPR.ino
+++ - CODE -/NewFolderForCode/Mk3Pcb/Functions/DRV8874PWPR/DRV8874PWPR.ino
@@ -1,14 +1,18 @@
 #include "DRV8874PWPR.h"
 
-DRV8874PWPR DRV8874PWPR_Obj('L');
+DRV8874PWPR DRV8874PWPR_Obj(5000, 8);
 
 void setup() {
 	Serial.begin(115200);
+	
+	// ESP32-S3 has 8 ledc channels starting from 0
+	DRV8874PWPR_Obj.LedcChannelLeft = 0;	// Add a ledc channel 
+	DRV8874PWPR_Obj.LedcChannelRight = 1;	// Add a ledc channel 
 }
 
 void loop() {
-	DRV8874PWPR_Obj.left(100);
-	DRV8874PWPR_Obj.right(-100);
+	DRV8874PWPR_Obj.left(100);		// Set speed to "100" out of "250" for the left driver
+	DRV8874PWPR_Obj.right(-100);	// Set speed to "-100" out of "250" for the right driver
 	delay(3000);
 	DRV8874PWPR_Obj.left(0);
 	DRV8874PWPR_Obj.right(0);
