2d0ccb75ab28633610f5e30afdbf274c546da5e3
havlian22 <122100034+havlian22@users.noreply.github.com>
2024-03-29 15:51:37 +0100

    přidán hlavičkový soubor pro piny a proměné

---
+++ - CODE -/MAIN_CODE/include/pinsAndVariables.h
@@ -0,0 +1,14 @@
+
+// motors pins:
+
+#define Nsleep 11
+#define Left_EN 14
+#define Left_PH 12
+#define Right_EN 21
+#define Right_PH 13
+
+// QRE pins:
+
+#define QRE_left 10
+#define QRE_right 9
+#define QRE_rear 8
