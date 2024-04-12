dc29bda044333b34ba83cac9d6fbe7b88e82954f
Jaroslav Bušek <nemotora@users.noreply.github.com>
2024-04-12 13:27:53 +0200

    Create Pins.h

---
+++ - CODE -/MAIN_CODE/include/Pins.h
@@ -0,0 +1,29 @@
+
+// motors pins:
+
+#define PIN_MOTOR_LEFT_NSLEEP   11
+#define PIN_MOTOR_LEFT_ENABLE   14
+#define PIN_MOTOR_LEFT_PHASE    12
+#define MOTOR_LEFT_CHANNEL      0
+
+#define PIN_MOTOR_RIGHT_NSLEEP  11
+#define PIN_MOTOR_RIGHT_ENABLE  21
+#define PIN_MOTOR_RIGHT_PHASE   13
+#define MOTOR_RIGHT_CHANNEL     1
+
+// QRE pins:
+
+#define QRE_left 10
+#define QRE_right 9
+#define QRE_rear 8
+
+// sharp pins:
+
+#define sharp_left 35
+#define sharp_right 16
+
+// leds pins:
+
+#define IRled 38
+#define Led_red 47
+#define Led_orange 48
