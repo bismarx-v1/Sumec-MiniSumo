be214ea4823458f5d601a775dcaf13197ce3d59b
Jaroslav Bušek <nemotora@users.noreply.github.com>
2024-04-12 13:28:13 +0200

    Update main.cpp

--- - CODE -/MAIN_CODE/src/main.cpp
+++ - CODE -/MAIN_CODE/src/main.cpp
@@ -1,133 +1,34 @@
-#include <Arduino.h>
-#include <IRremote.h>
-#include "pinsAndVariables.h"
-#include "QRE1113.h"            //befor update
-#include "DRV8874PWPR.h"        //befor update
-#include "RC-5.h"
+#include <Pins.h>
+#include <Motor.h>
 
+Motor MotorLeft(PIN_MOTOR_LEFT_NSLEEP,
+                PIN_MOTOR_LEFT_ENABLE,
+                PIN_MOTOR_LEFT_PHASE,
+                MOTOR_LEFT_CHANNEL);
+Motor MotorRight(PIN_MOTOR_RIGHT_NSLEEP,
+                 PIN_MOTOR_RIGHT_ENABLE,
+                 PIN_MOTOR_RIGHT_PHASE,
+                 MOTOR_RIGHT_CHANNEL);
 
+void setup() {
 
-// Objects
-
-//motors objects
-H_bridge Motors(5000,8);
-
-//qre objects
-QRE qreLeft('L');
-QRE qreRight('R');
-QRE qreBack('B');
-
-
-//TICKs 
-
-TICK Tick_A;
-
-
-
-void setup() 
-{
-    Serial.begin(115200);
-    Tick_A.tickTime = 10; 
-    irrecv.enableIRIn();
-
-    IRstart();
-     
 }
 
-
-void loop()
-{
-    //=========================Writeing value in sensors to variables=============
-
-    QREleft = qreLeft.get();
-    QREright = qreRight.get();
-    QREback = qreBack.get();
-
-    //Writeing value to TICK
-
-    Tick_A.tickNumber = Tick_managing(Tick_A.tickTime, Tick_A.tickNumber, Tick_A.lastTick);
-    Tick_A.lastTick = Tick_lastManaging(Tick_A.tickTime, Tick_A.tickNumber, Tick_A.lastTick);
-
-    //==========================Rafinering inputs==============================
-
-
-    if(state == 0)
-    {
-        
-        if(QREleft == 1 && QREright != 1)
-        {
-            state = 1;
-
-            Tick_A.lastTick = millis();
-            Tick_A.tickNumber = 0;
-        }
-        else if(QREright == 1 && QREleft != 1)
-        {
-            state = 2;
-
-            Tick_A.lastTick = millis();
-            Tick_A.tickNumber = 0;
-        }
-        else if(QREback == 1 )
-        {
-            state = 0;
-        }
-        else
-        {
-            state = 0;
-        }
-    }
-
-    //===========================Procesing resoluts - states===============================
-
-    
-
-    switch(state)
-    {
-        case 0:
-            Motors.left(255);
-            Motors.right(255);
-            break;
-        case 1:
-
-            if(Tick_A.tickNumber < 10)
-            {
-                Motors.left(-255);
-                Motors.right(-255);
-            }
-            else if(Tick_A.tickNumber < 30 )
-            {
-                Motors.left(255);
-                Motors.right(-255);
-            }
-
-            else
-            {
-                state = 0;
-
-            }
-
-            break;
-        case 2:
-
-            if(Tick_A.tickNumber < 10)
-            {
-                Motors.left(-255);
-                Motors.right(-255);
-            }
-            else if(Tick_A.tickNumber < 30 )
-            {
-                Motors.left(-255);
-                Motors.right(255);
-            }
-            else
-            {
-                state = 0;
-            }
-
-            break;
-
-    }
-
-    
+void loop() {
+    MotorLeft.goForward();
+    MotorLeft.setSpeed(0.5);
+    MotorRight.goBackward();
+    MotorRight.setSpeed(0.5);
+    delay(2000);
+    MotorLeft.stop();
+    MotorRight.stop();
+    delay(2000);
+    MotorLeft.goBackward();
+    MotorLeft.setSpeed(0.5);
+    MotorRight.goForward();
+    MotorRight.setSpeed(0.5);
+    delay(2000);
+    MotorLeft.stop();
+    MotorRight.stop();
+    delay(2000);
 }
