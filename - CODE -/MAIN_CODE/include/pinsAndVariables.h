256ea5494e4f71b90e578a41e4089e785e509d0f
Jaroslav Bušek <nemotora@users.noreply.github.com>
2024-04-12 13:28:07 +0200

    Update pinsAndVariables.h

--- - CODE -/MAIN_CODE/include/pinsAndVariables.h
+++ - CODE -/MAIN_CODE/include/pinsAndVariables.h
@@ -1,31 +1,3 @@
-
-// motors pins:
-
-#define Nsleep 11
-#define Left_EN 14
-#define Left_PH 12
-#define Right_EN 21
-#define Right_PH 13
-
-// QRE pins:
-
-#define QRE_left 10
-#define QRE_right 9
-#define QRE_rear 8
-
-// sharp pins:
-
-#define sharp_left 35
-#define sharp_right 16
-
-// leds pins:
-
-#define IRled 38
-#define Led_red 47
-#define Led_orange 48
-
-
-
 //============================= PROGRAM VARIABLES ===============================
 
 uint8_t state;
@@ -74,10 +46,3 @@ int LUNAmiddle;
 
 bool SHARPleft;
 bool SHARPright;
-
-
-//Motors
-
-int16_t motorLeft;
-int16_t motorRight;
-
