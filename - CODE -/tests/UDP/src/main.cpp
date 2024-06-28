e3ba96ebdfa33b1a35867bc48da593ac99d1d714
Natani Simak <matej.simacek.2@gmail.com>
2024-06-28 18:21:12 +0200

    last save before release

--- - CODE -/tests/UDP/src/main.cpp
+++ - CODE -/tests/UDP/src/main.cpp
@@ -1,28 +1,21 @@
 #include <Arduino.h>
 #include "UDPStuff.h"
 
+uint8_t ReturnSize;
 char ReturnMessage[bufferLen];
 IPAddress ReturnIp;
-uint8_t ReturnSize;
+
 const uint64_t TimePeriodSend = 5000;
 uint64_t TimeLastSend = 0;
 
 void setup() {
 	Serial.begin(115200);
 
-	AP.InitAP("SUDPRCP_U#1", "018f34a5-6daa-7729-8ed0-884b8a7c6c45", IPAddress(192,168,1,22));	// 192.168.1.22
-
-	udp.begin(UdpPort);
-
-	WiFi.onEvent(StationConnectedToAP, ARDUINO_EVENT_WIFI_AP_STACONNECTED);			// add callback to "station connected to ap" event
-	WiFi.onEvent(StationDisconnectedFromAP, ARDUINO_EVENT_WIFI_AP_STADISCONNECTED);	// add the same callback to "station disconected from ap" event
-
-	delay(5000);
+	UDP_Setup();
 }
 
 void loop() {
-	
-	CheckIfRecieved(&ReturnSize, ReturnMessage, &ReturnIp);
+	UDP_CheckIfRecieved(&ReturnSize, ReturnMessage, &ReturnIp);	// use this to check for new messages
 	if(ReturnSize > 0) {
 		Serial.print("UDP recieved: ");
 		Serial.print(ReturnMessage);
@@ -32,7 +25,7 @@ void loop() {
 	
 	if(millis()-TimePeriodSend >= TimeLastSend) {
 		TimeLastSend = millis();
-		SendUdpToAll("Test1", 5);
+		UDP_SendUdpToAll("Test1", 5);	// sends string "Test1" 5 times to all connected devices 
 	}
 
 	delay(100);
